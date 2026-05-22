99f8a3c7fe6f1a27be313b31530eff948218a1cb
karbonpanzer <74460271+karbonpanzer@users.noreply.github.com>
2026-05-22 09:50:55 -0400

    Comp Fixes

--- Source/EOTF.Core/Comps/CompEdit_Decal.cs
+++ Source/EOTF.Core/Comps/CompEdit_Decal.cs
@@ -1,42 +1,44 @@
 using System.Collections.Generic;
-using HarmonyLib;
 using RimWorld;
-using Verse;
 using UnityEngine;
+using Verse;
 
 namespace EOTF.Core.DecalSystem
 {
-    public sealed class CompEditDecalMarker : ThingComp 
+    public sealed class CompEditDecalMarker : ThingComp
     {
         public DecalProfileSet ProfileSet = DecalProfileSet.Default;
 
-        // XML save/load for decal state, this is the fallback structure if the symboldefs shit the bed
+        //Saves decal state per-item so it persists across saves
         public override void PostExposeData()
         {
             base.PostExposeData();
-            
+
             Scribe_Values.Look(ref ProfileSet.Helmet.Active, "eotfDecalHelmetActive");
             Scribe_Values.Look(ref ProfileSet.Helmet.SymbolPath, "eotfDecalHelmetPath", "");
             Scribe_Values.Look(ref ProfileSet.Helmet.SymbolColor, "eotfDecalHelmetColor", Color.white);
-            
+
             Scribe_Values.Look(ref ProfileSet.Armor.Active, "eotfDecalArmorActive");
             Scribe_Values.Look(ref ProfileSet.Armor.SymbolPath, "eotfDecalArmorPath", "");
             Scribe_Values.Look(ref ProfileSet.Armor.SymbolColor, "eotfDecalArmorColor", Color.white);
         }
 
-        //Hook into the WorldComponent so pawn tracking doesn't break when gear changes
+        //Register with WorldComponent when equipped so the cache knows about this pawn
         public override void Notify_Equipped(Pawn pawn)
         {
             base.Notify_Equipped(pawn);
             WorldComponentDecalPawns.Instance?.Register(pawn);
         }
 
+        //Only unregister if no other decal gear is still on the pawn
         public override void Notify_Unequipped(Pawn pawn)
         {
             base.Notify_Unequipped(pawn);
-            foreach (var apparel in pawn.apparel.WornApparel)
+            List<Apparel> wornApparel = pawn.apparel.WornApparel;
+            for (int i = 0; i < wornApparel.Count; i++)
             {
-                if (apparel.TryGetComp<CompEditDecalMarker>() != null) return;
+                if (wornApparel[i].def.HasComp<CompEditDecalMarker>())
+                    return;
             }
             WorldComponentDecalPawns.Instance?.Unregister(pawn);
         }
@@ -46,39 +48,4 @@ namespace EOTF.Core.DecalSystem
     {
         public CompPropertiesEditDecalMarker() => compClass = typeof(CompEditDecalMarker);
     }
-    
-    //Gizmo that opens the decal editing UI, still needs work
-    [StaticConstructorOnStartup]
-    [HarmonyPatch(typeof(Pawn), nameof(Pawn.GetGizmos))]
-    public static class PatchPawnGetGizmosDecals
-    {
-        private static readonly Texture2D GizmoIcon =
-            ContentFinder<Texture2D>.Get("UI/CustomizeDecal");
-
-        public static void Postfix(Pawn __instance, ref IEnumerable<Gizmo> __result)
-        {
-            if (__instance.Faction != Faction.OfPlayerSilentFail || 
-                !DecalUtil.PawnHasAnyDecalApparel(__instance)) return;
-        
-            __result = AppendGizmo(__result, CreateDecalGizmo(__instance));
-        }
-
-        private static IEnumerable<Gizmo> AppendGizmo(IEnumerable<Gizmo> source, Gizmo gizmo)
-        {
-            foreach (var g in source) yield return g;
-            yield return gizmo;
-        }
-
-        private static Gizmo CreateDecalGizmo(Pawn pawn)
-        {
-            return new Command_Action
-            {
-                defaultLabel = "EOTF_StyleDecalsGizmo".Translate(pawn.LabelCap),
-                defaultDesc = "EOTF_StyleDecalsDesc".Translate(),
-                icon = GizmoIcon,
-                action = () => Find.WindowStack.Add(new DialogEditDecals(pawn))
-            };
-        }
-    }
-    
 }
